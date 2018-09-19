d690b94dc9a5ecc87c400cfb3f4af6dba9c12f9e
Celil Bozkurt <hi@celil.me>
2018-09-19 19:52:09 +0300

    - spellcheck

--- app/Middleware/AuthMiddleware.php
+++ app/Middleware/AuthMiddleware.php
@@ -8,7 +8,7 @@ use App\Models\AppData;
 class AuthMiddleware {
 
 	public function __invoke($request, $response, $next) {
-		$apiKey = $request->headers->get('apiKey');
+		$apiKey = $app->request->headers->get('apiKey');
 		$exist = AppData::where('api_key', $api_key)->exists();
 
 		if (!$exist) {
