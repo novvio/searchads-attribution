c45bd7b348d5b435f28e0618c94fbdb55d428a40
Celil Bozkurt <hi@celil.me>
2018-09-19 19:46:16 +0300

    * apiKey added to headers.

--- app/Middleware/AuthMiddleware.php
+++ app/Middleware/AuthMiddleware.php
@@ -8,12 +8,10 @@ use App\Models\AppData;
 class AuthMiddleware {
 
 	public function __invoke($request, $response, $next) {
-		$params = $request->getparams();
-
-		$apiKey = $params['apiKey'];
+		$apiKey = $request->headers->get('apiKey');
 		$exist = AppData::where('api_key', $api_key)->exists();
 
-		if ($exist) {
+		if (!$exist) {
 			$responseMessage = ['ss' => 'sd'];
 			$response->withHeader('Content-Type', 'application/json');
 			$response->withStatus(401);
