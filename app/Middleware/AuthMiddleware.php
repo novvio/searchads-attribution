d17f9cb5e11f7e3dca9d2d32a5e76737ea816330
Celil Bozkurt <hi@celil.me>
2018-09-19 20:21:08 +0300

    * header config completed.

--- app/Middleware/AuthMiddleware.php
+++ app/Middleware/AuthMiddleware.php
@@ -8,7 +8,7 @@ use App\Models\AppData;
 class AuthMiddleware {
 
 	public function __invoke($request, $response, $next) {
-		$apiKey = $request->getHeader('Authorization');
+		$apiKey = $request->getHeader('Authorization')[0];
 		$exist = AppData::where('api_key', $api_key)->exists();
 
 		if (!$exist) {
