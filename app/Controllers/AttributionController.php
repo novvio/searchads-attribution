fecb577ee70762c23cc521a2accddd163735af53
Celil Bozkurt <hi@celil.me>
2018-09-19 17:27:40 +0300

    - todayCampaigns completed. - todayAdgroups completed.

--- app/Controllers/AttributionController.php
+++ app/Controllers/AttributionController.php
@@ -33,7 +33,7 @@ class AttributionController {
 	}
 
 	public function getTodayCampaigns($request, $response) {
-		$trends = Capsule::table('attributions')
+		$todayCampaigns = Capsule::table('attributions')
 					->select('campaign_id','campaign_name', Capsule::raw('count(*) as total'))
 					->where('updated_at', '>=', Carbon::today())
 					->groupBy('campaign_id', 'campaign_name')
@@ -42,23 +42,23 @@ class AttributionController {
 
 		$responseMessage = [
 			'status' => 'Success',
-			'trends' => $trends
+			'todayCampaigns' => $todayCampaigns
 		];
 
 		return $response->withJson($responseMessage, 200);
 	}
 
 	public function getTodayAdGroups($request, $response) {
-		$trends = Capsule::table('attributions')
-					->select('adgroup_id', Capsule::raw('count(*) as total'))
+		$todayAdGroups = Capsule::table('attributions')
+					->select('adgroup_id', 'adgroup_name', Capsule::raw('count(*) as total'))
 					->where('updated_at', '>=', Carbon::today())
-					->groupBy('adgroup_name')
+					->groupBy('adgroup_id', 'adgroup_name')
 					->orderBy('total', 'desc')
 					->get();
 					
 		$responseMessage = [
 			'status' => 'Success',
-			'trends' => $trends
+			'todayAdGroups' => $todayAdGroups
 		];
 
 		return $response->withJson($responseMessage, 200);
